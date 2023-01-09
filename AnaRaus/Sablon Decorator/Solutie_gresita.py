2c21e61967a0df2397ca9e621ec7d1c67a71c4ca
cojocariudaniel1 <cojocariudaniel02@gmail.com>
2023-01-09 13:51:09 +0200

    Commit

--- AnaRaus/Sablon Decorator/Solutie_gresita.py
+++ AnaRaus/Sablon Decorator/Solutie_gresita.py
@@ -1,24 +1,16 @@
-def display_decorator(func):
-    def wrapper(self):
-        mesaje = {
-            "stock": ("Produs in stoc", "green"),
-            "inspectie": ("Produs in inspectie", "red"),
-            "rebut": ("Produs rebut", "blue"),
-            "blocat": ("Produs blocat", "yellow")
-        }
-        mesaj, culoare = mesaje.get(self.stare_produs, ("Stare produs necunoscuta", "white"))
-        print(f"{culoare} {mesaj}")
-        func(self)
-
-    return wrapper
-
-
 class Produs:
     def __init__(self, stare_produs: str = "stock"):
         self.stare_produs = stare_produs
 
-    @display_decorator
     def display(self):
+        mesaje = {
+            "stock": ("Produsul este in stoc", "green"),
+            "inspectie": ("Produsul este in curs de inspectie", "yellow"),
+            "rebut": ("Produsul este rebut", "red"),
+            "blocat": ("Produsul este blocat", "blue"),
+        }
+        mesaj, culoare = mesaje.get(self.stare_produs, ("Stare produs necunoscuta", "white"))
+        print(f"{culoare} {mesaj}")
         print("Display Successful")
 
 
